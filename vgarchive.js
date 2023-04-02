d66b9adfdb1cd4dd32db0b745da6bdd61ca9db46
Abel Zaragosa <124743424+obsolete2099@users.noreply.github.com>
2023-04-02 14:37:08 -0500

    updated javascript for changes
    javascript changes for the manual update page. Not as slick as I thought as I was on the backend, so if I could have some help with that it would be really helpful. Going to create a homepage that navigates to the login and registration pages.

--- vgarchive.js
+++ vgarchive.js
@@ -1,27 +1,77 @@
-var vgTimeSpent = window.prompt("Please Enter Time Spent Playing:");
-var vgRecommend = window.prompt("Would you Recommend this Title?");
-var vgGivenUp = window.prompt("Have you Stopped Playing the Game?");  
-  
+$(document).ready(function() {
+  // Set up an empty array to hold the game collection
+  var gameCollection = [];
 
-  
-  
-  
-      c1.innerText = vgName
-      c2.innerText = vgConsole
-      c3.innerText = vgStatus
-      c4.innerText = vgTimeSpent
-      c5.innerText = vgRecommend
-      c6.innerText = vgGivenUp
-  
-  
-  
-      row.appendChild(c1);
-      row.appendChild(c2);
-      row.appendChild(c3);
-      row.appendChild(c4);
-      row.appendChild(c5);
-      row.appendChild(c6);
-      
-      // Append row to table body
-      table.appendChild(row)
-}
+  // Function to add a game to the collection
+  function addGame(name, genre, console, completed, recommend) {
+    gameCollection.push({
+      name: name,
+      genre: genre,
+      console: console,
+      completed: completed,
+      recommend: recommend
+    });
+    updateTable();
+  }
+
+  // Function to remove a game from the collection
+  function removeGame(index) {
+    gameCollection.splice(index, 1);
+    updateTable();
+  }
+
+  // Function to update the table with the current game collection
+  function updateTable() {
+    // Clear the current table contents
+    $('#game-table tbody').empty();
+
+    // Loop through the game collection and add each game to the table
+    for (var i = 0; i < gameCollection.length; i++) {
+      var game = gameCollection[i];
+      var row = '<tr>' +
+                '<td>' + game.name + '</td>' +
+                '<td>' + game.genre + '</td>' +
+                '<td>' + game.console + '</td>' +
+                '<td>' + (game.completed ? 'Yes' : 'No') + '</td>' +
+                '<td>' + (game.recommend ? 'Yes' : 'No') + '</td>' +
+                '<td><button class="edit" data-index="' + i + '">Edit</button> ' +
+                '<button class="delete" data-index="' + i + '">Delete</button></td>' +
+                '</tr>';
+      $('#game-table tbody').append(row);
+    }
+  }
+
+  // Add a submit handler for the game form
+  $('#game-form').submit(function(event) {
+    event.preventDefault();
+    var name = $('#name').val();
+    var genre = $('#genre').val();
+    var console = $('#console').val();
+    var completed = $('#completed').is(':checked');
+    var recommend = $('#recommend').is(':checked');
+    addGame(name, genre, console, completed, recommend);
+    $('#name').val('');
+    $('#genre').val('');
+    $('#console').val('');
+    $('#completed').prop('checked', false);
+    $('#recommend').prop('checked', false);
+  });
+
+  // Add a click handler for the delete buttons
+  $('#game-table').on('click', '.delete', function() {
+    var index = $(this).data('index');
+    removeGame(index);
+  });
+
+  // Add a click handler for the edit buttons
+  $('#game-table').on('click', '.edit', function() {
+    var index = $(this).data('index');
+    var game = gameCollection[index];
+    $('#name').val(game.name);
+    $('#genre').val(game.genre);
+    $('#console').val(game.console);
+    $('#completed').prop('checked', game.completed);
+    $('#recommend').prop('checked', game.recommend);
+    removeGame(index);
+  });
+});
